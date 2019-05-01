ad72823e2751f7acaff6cce93f49ec6da4762b49
wsh <1747693902@qq.com>
2019-05-01 22:34:15 +0800

    初步
    Signed-off-by: wsh <1747693902@qq.com>

---
+++ js/sub.js
@@ -0,0 +1,13 @@
+	
+	mui('.mui-bar-tab').on('tap', 'a', function(e) {
+		$(this).addClass(' mui_color').siblings('a').removeClass('mui_color');		
+		$()
+	});
+	
+	mui('.mui-bar-tab').on('tap', '.pic', function(e) {
+		console.log(111);
+})
+
+
+
+
