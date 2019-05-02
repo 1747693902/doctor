d27f16ec381ccdb348139f3ea5751a78f9017972
wsh <1747693902@qq.com>
2019-05-02 14:18:18 +0800

    two
    Signed-off-by: wsh <1747693902@qq.com>

--- js/sub.js
+++ js/sub.js
@@ -1,13 +1,23 @@
 	
 	mui('.mui-bar-tab').on('tap', 'a', function(e) {
 		$(this).addClass(' mui_color').siblings('a').removeClass('mui_color');		
-		$()
+		
 	});
 	
 	mui('.mui-bar-tab').on('tap', '.pic', function(e) {
-		console.log(111);
+//		console.log(111);
+		var src = document.getElementById('picmy');
+		var srchome = document.getElementById('pichome');
+		src.src= 'imgs/ic_mine_n.png';
+		srchome.src= 'imgs/ic_home_s.png';
 })
 
 
-
+	mui('.mui-bar-tab').on('tap', '.picmy', function(e) {
+//		console.log(111);
+		var srchome = document.getElementById('pichome');
+		var src = document.getElementById('picmy');
+		srchome.src= 'imgs/ic_home_n.png';
+		src.src= 'imgs/ic_mine_s.png';
+})
 
---
+++ js/sub1/sub1.js
@@ -0,0 +1,7 @@
+//弹性窗口
+(function($){
+        $(".mui-scroll-wrapper").scroll({
+              bounce: true,//滚动条是否有弹力默认是true
+              indicators:false, //是否显示滚动条,默认是true
+        });
+})(mui);
